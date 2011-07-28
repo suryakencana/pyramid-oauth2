724ce9ae8af698a28fb74674138e7927d8a38912
kevin@tick.ee <kevin@tick.ee@7eae23f6-8020-8c89-1981-dfee3d3eb278>
2011-07-28 09:37:38 +0000

    Made the handler aware of the client_id via the ``requestor_id`` attribute

--- pyramid_oauth2/decorator.py
+++ pyramid_oauth2/decorator.py
@@ -8,7 +8,7 @@ from pyramid_oauth2.oauth2.datastore import OAuth2DataStore
 from pyramid_oauth2.oauth2.errorhandling import OAuth2ErrorHandler
 from pyramid_oauth2.resources.request import OAuth2Request
 
-def oauth2(allowed_scope):
+def oauth2(allowed_scope=[]):
     def wrap(view_function):
         def new_function(*args, **kw):
             handler = args[0]
@@ -18,8 +18,10 @@ def oauth2(allowed_scope):
             if handler.request.access_token:
                 access_token = handler.request.access_token.get('token')
                 datastore = OAuth2DataStore()
-                valid = datastore.validate_access_token(access_token, allowed_scope)
+                valid, client_id = datastore.validate_access_token(access_token, allowed_scope)
                 if valid:
+                    # Add client id to the request and execute view
+                    setattr(handler, 'requestor_id', client_id)
                     return view_function(*args, **kw)
                 # No token found or token expired.
                 else:
